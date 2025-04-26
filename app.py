a32fe42eaf50e35f9763774d0eaa262646e11645
drmansor <drmansourajarmah@gmail.com>
2025-04-26 03:40:07 -0700

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def detect_emotion():
         'emotion': emotion,
         'confidence': confidence
     })
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))  # this line is very important
     app.run(host="0.0.0.0", port=port)
+
